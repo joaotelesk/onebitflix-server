c9be9141326ab20094e4eac58b2995712a3e1457
Joao Teles <joaojoseteles@gmail.com>
2022-10-27 18:40:13 -0300

    feat: endpoint courses-3 cursos em destaque

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -20,4 +20,16 @@ export const courseService = {
     });
     return courseWithEpisodes;
   },
+  getrandomFeaturedCourses: async () => {
+    const featuredCourses = await Course.findAll({
+      attributes: ["id", "name", "synopsis", ["thumbnail_url", "thumbnailUrl"]],
+      where: {
+        featured: true,
+      },
+    });
+    const randomFeaturedCourses = featuredCourses.sort(
+      () => 0.5 - Math.random()
+    );
+    return randomFeaturedCourses.slice(0, 3);
+  },
 };
