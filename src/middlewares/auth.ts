e5e5cad31354dd2f43c27747ba20ffbd10d8acc2
Joao Teles <joaojoseteles@gmail.com>
2022-11-28 17:34:34 -0300

    :sparkles: feat: Middleware de autorizaçao

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,31 @@
+import { NextFunction, request, Request, Response } from "express";
+import { JwtPayload } from "jsonwebtoken";
+import { UserInstance } from "../models/User";
+import { jwtService } from "../services/jwtService";
+import { userService } from "../services/userService";
+
+export interface AuthenticatedRequest extends Request {
+  user?: UserInstance | null;
+}
+
+export function ensureAuth(
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const authorizationHeader = req.headers.authorization;
+  if (!authorizationHeader)
+    return res.status(401).json({
+      message: "Não autorizado: nunhum token foi encontrado",
+    });
+  const token = authorizationHeader.replace(/Bearer /, "");
+  jwtService.verifyToken(token, (err, decoded) => {
+    if (err || typeof decoded === "undefined")
+      return res
+        .status(401)
+        .json({ messege: "Não Autorizado: token inválido" });
+    userService.findByEmail((decoded as JwtPayload).email).then((user) => {
+      (req.user = user), next();
+    });
+  });
+}
