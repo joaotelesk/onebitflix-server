c9a56dce370620ce93635109d2d63d6995da81ea
Joao Teles <joaojoseteles@gmail.com>
2022-11-28 18:21:53 -0300

    :recycle: refactor: Middleware de proteçao de video

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -16,16 +16,40 @@ export function ensureAuth(
   const authorizationHeader = req.headers.authorization;
   if (!authorizationHeader)
     return res.status(401).json({
-      message: "Não autorizado: nunhum token foi encontrado",
+      message: "Não autorizado: nenhum token foi encontrado",
     });
   const token = authorizationHeader.replace(/Bearer /, "");
-  jwtService.verifyToken(token, (err, decoded) => {
+  jwtService.verifyToken(token, async (err, decoded) => {
     if (err || typeof decoded === "undefined")
       return res
         .status(401)
         .json({ messege: "Não Autorizado: token inválido" });
-    userService.findByEmail((decoded as JwtPayload).email).then((user) => {
-      (req.user = user), next();
+    const user = await userService.findByEmail((decoded as JwtPayload).email);
+    next();
+  });
+}
+
+export function ensureAuthViaQuery(
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const { token } = req.query;
+  if (!token)
+    return res.status(401).json({
+      message: "Não autorizado: nenhum token foi encontrado",
     });
+  if (typeof token !== "string")
+    return res.status(401).json({
+      message: "O parâmentro token deve serdo tipo string",
+    });
+  jwtService.verifyToken(token, async (err, decoded) => {
+    if (err || typeof decoded === "undefined")
+      return res
+        .status(401)
+        .json({ messege: "Não Autorizado: token inválido" });
+    const user = await userService.findByEmail((decoded as JwtPayload).email);
+    req.user = user;
+    next();
   });
 }
