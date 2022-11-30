44cd3ec6465f3be146933fcc1cf06daced0edb13
Joao Teles <joaojoseteles@gmail.com>
2022-11-30 13:50:23 -0300

    :bug: fix: associaçao de curso com episodios linha:11

--- src/models/index.ts
+++ src/models/index.ts
@@ -8,6 +8,7 @@ import { Like } from "./Like";
 Category.hasMany(Course);
 
 Course.belongsTo(Category);
+Course.hasMany(Episode);
 Course.belongsToMany(User, { through: Favorite });
 Course.belongsToMany(User, { through: Like });
 Course.hasMany(Favorite, { as: "favoritesUsers", foreignKey: "course_id" });
